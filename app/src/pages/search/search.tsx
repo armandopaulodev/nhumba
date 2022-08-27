4bf787a38cd2b8bf928da40bd91dd5973436d45f
Armando Paulo <51958179+Aptrue@users.noreply.github.com>
2022-08-27 12:21:07 +0200

    little change on search_page

--- app/src/pages/search/search.tsx
+++ app/src/pages/search/search.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { VStack, NativeBaseProvider,Icon, Box, Divider,Input, Heading, Text, Center } from 'native-base';
+import { VStack, NativeBaseProvider,Icon, Box, Divider,Input, Heading, Text, Center, HStack } from 'native-base';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 
@@ -11,10 +11,14 @@ export default function Search(){
 
 return (
         <NativeBaseProvider>
-                          <VStack w="100%" space={5} alignSelf="center">
-                            <Heading fontSize="lg">Cupertino</Heading>
-                            <Input placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="2" borderWidth="1" borderColor="black"
-                             InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="ios-search" />} />} />
+                          <VStack w="100%" space={5} alignSelf="center" mt={10}>
+
+                                    <Box mx={5}>
+                                    <Heading color="silver" fontSize={18} fontFamily='Roboto'>Buscar imóveis</Heading>
+                                    <Input placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="2" borderWidth="1" borderColor="black"
+                                    InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="ios-search" />} />} />
+                                    </Box>
+
                          </VStack>
         </NativeBaseProvider>
 );
