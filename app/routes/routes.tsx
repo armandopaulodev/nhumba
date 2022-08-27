260c48cfa335918edf528c50a0928c7821d3a677
Armando Paulo <51958179+Aptrue@users.noreply.github.com>
2022-08-27 11:38:15 +0200

    tabsRoute add

---
+++ app/routes/routes.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Home from '../src/pages/home/home';
+
+export default function TabsNavigation() {
+
+    const Tab = createBottomTabNavigator();
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Buscar') {
+              iconName = focused ? 'ios-search' : 'ios-search-outline';
+            } else if(route.name=='Perfil'){
+              iconName = focused ? 'ios-person' : 'ios-person-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} style={{ paddingBottom: 5 }} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'tomato',
+        })}
+        >
+          <Tab.Screen name="Home" component={Home} options={ {headerShown: false}}  />
+          <Tab.Screen name="Buscar" component={Home} />
+          <Tab.Screen name="Perfil" component={Home} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
