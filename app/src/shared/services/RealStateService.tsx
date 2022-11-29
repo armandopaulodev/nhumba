a3f810c19ce9a2b83335d7988d048c965955bb01
Armando Paulo <51958179+armandopaulodev@users.noreply.github.com>
2022-11-29 14:22:06 +0200

    service little changes

--- app/src/shared/services/RealStateService.tsx
+++ app/src/shared/services/RealStateService.tsx
@@ -3,9 +3,12 @@ import React from "react";
 
 
 export class RealState {
-    url ='http://192.168.133.187:8000/api/realstate/all';
-    url_store = 'http://192.168.133.187:8000/api/realstate/store';
-    url_realtor ='http://192.168.133.187:8000/api/realtor/all';
+    
+    ip = '192.168.133.187';
+
+    url ='http://'+this.ip+':8000/api/realstate/all';
+    url_store = 'http://'+this.ip+':8000/api/realstate/store';
+    url_realtor ='http://'+this.ip+':8000/api/realtor/all';
 
 
    // 192.168.133.187 troque esse endereco pelo endereco da sua rede. o celular e pc devem estar na mesma rede
